93bf15805d7f1954ccf579e164ee170d57adccb8
BintangP28 <77977048+BintangP28@users.noreply.github.com>
2021-02-01 13:54:29 +0700

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -1,8 +1,8 @@
 let fetch = require('node-fetch')
 
 let handler = async(m, { conn, args, usedPrefix }) => {
-    if (args.length == 0) return conn.reply(m.chat, `Untuk menggunakan ${usedPrefix}anime\nSilahkan ketik: ${usedPrefix}anime [query]\nContoh: ${usedPrefix}anime random\n\nquery yang tersedia:\nrandom, waifu, husbu, neko`, m)
-    if (args[0] == 'random' || args[0] == 'waifu' || args[0] == 'husbu' || args[0] == 'neko') {
+    if (args.length == 0) return conn.reply(m.chat, `Untuk menggunakan ${usedPrefix}anime\nSilahkan ketik: ${usedPrefix}anime [query]\nContoh: ${usedPrefix}anime random\n\nquery yang tersedia:\nrandom, waifu, husbu, neko, hentai`, m)
+    if (args[0] == 'random' || args[0] == 'waifu' || args[0] == 'husbu' || args[0] == 'neko' || args[0] == 'hentai') {
 
         fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/' + args[0] + '.txt')
             .then(res => res.text())
@@ -12,7 +12,7 @@ let handler = async(m, { conn, args, usedPrefix }) => {
                 conn.sendFile(m.chat, randomnimex, '', 'Jangan apa apain tante ku om:D', m)
             })
             .catch(() => {
-                conn.reply(m.chat, 'Ada yang Error cuy... Bisa tanyakan ke\n*Instagram:* @ff.kelvin15!', m)
+                conn.reply(m.chat, 'Ada yang Error cuy... Bisa tanyakan ke\n*Instagram:* @bintangp_28!', m)
             })
     } else {
         conn.reply(m.chat, `Maaf query tidak tersedia. Silahkan ketik ${usedPrefix}anime untuk melihat list query`, m)
